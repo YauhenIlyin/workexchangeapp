21611c79d95bbef330084aa5a761fddf6b85b740
Yauhen Ilyin <prog.jekylin@gmail.com>
2021-08-30 12:53:36 +0300

    UserDaoImpl fix

--- src/main/java/by/ilyinyauhen/workexchangeapp/model/dao/impl/UserDaoImpl.java
+++ src/main/java/by/ilyinyauhen/workexchangeapp/model/dao/impl/UserDaoImpl.java
@@ -11,14 +11,32 @@ import java.util.List;
 
 public class UserDaoImpl implements UserDao {
 
-    private static final String USERS_TABLE_NAME = "users";
-    private static final String SQL_EXPRESSION_FIND_ALL_USERS = """
-            SELECT users.id, users.first_name, users.last_name, users.registration_date, 
-            users.last_activity_date, users.e_mail, users.mobile_number, 
-            (SELECT user_role.role_description FROM user_role WHERE user_role.id = users.user_role_id ), 
-            (SELECT account_status.account_status_description FROM account_status WHERE account_status.id = users.account_status_id)
+    private static final String BASE_USER_ROLE_DESCRIPTION = "simple_user";
+    private static final String BASE_USER_ACCOUNT_STATUS_DESCRIPTION = "waiting_activation";
+    private static final String PS_SQL_EXPRESSION_FIND_ALL_USERS = """
+            SELECT id, first_name, last_name, registration_date, 
+            last_activity_date, e_mail, mobile_number, 
+            (SELECT role_description FROM user_role WHERE user_role.id = users.user_role_id ), 
+            (SELECT account_status_description FROM account_status WHERE account_status.id = users.account_status_id)
+            FROM users;
+            """; //todo
+    private static final String PS_SQL_EXPRESSION_FIND_USER_BY_ID = """
+            SELECT id, first_name, last_name, registration_date, 
+            last_activity_date, e_mail, mobile_number, 
+            (SELECT role_description FROM user_role WHERE user_role.id = users.user_role_id ), 
+            (SELECT account_status_description FROM account_status WHERE account_status.id = users.account_status_id)
             FROM users
+            WHERE id = ?;         
             """;
+    private static final String PS_SQL_EXPRESSION_CREATE_USER = """
+            INSERT INTO users(first_name, last_name, registration_date,
+            last_activity_date, e_mail, mobile_number, user_role_id, account_status_id)
+            VALUES (
+            ?,?,?,?,?,?,
+            (SELECT user_role.id FROM user_role WHERE user_role.role_description = ""),
+            (SELECT account_status.account_status_description FROM account_status WHERE account_status.id = users.account_status_id)
+            );
+            """; //todo
 
     @Override
     public List<User> findAll() throws DaoException {
@@ -30,6 +48,14 @@ public class UserDaoImpl implements UserDao {
         return null;
     }
 
+    public boolean validateAccountLogin(char[] login) {
+        return false;
+    }
+
+    public boolean createEntity(User user, char[] login, char[] password) throws DaoException {
+        return false;
+    }
+
     @Override
     public boolean deleteEntity(User user) throws DaoException {
         return false;
@@ -37,10 +63,6 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public boolean deleteEntityById(Long id) throws DaoException {
-        return false;
-    }
-
-    public boolean createEntity(User user, char[] login, char[] password) throws DaoException {
         return false;
     }
 
